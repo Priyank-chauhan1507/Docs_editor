aeadab6d30f4a4736a07e3ef4aa16d7c32e28080
priyank chauhan <priyankchauhan@priyanks-MacBook-Air.local>
2024-05-08 16:45:09 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
-function App() {
-  return (
-    <div className="">
-      hello docs_editor
-    </div> 
-  );
+const App=()=>{
+  const [value, setValue] = useState('sdklufhdsiu');
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ size: [] }],
+      [{ font: [] }],
+      [{ align: ["right", "center", "justify"] }],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["link", "image"],
+      [{ color: ["red", "#785412"] }],
+      [{ background: ["red", "#785412"] }]
+    ]
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "link",
+    "color",
+    "image",
+    "background",
+    "align",
+    "size",
+    "font"
+  ];
+
+
+  return <><div style={{width:"80%"}}><ReactQuill theme="snow" value={value} onChange={setValue} modules={modules}
+  formats={formats} 
+  /> </div>
+  <div></div>
+  </>
 }
 
 export default App;
